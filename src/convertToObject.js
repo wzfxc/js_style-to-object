f138adda5ee50486049c6bd2a970bb568b290135
wzfxc <maksohrimenko1@gmail.com>
2025-09-19 18:02:42 +0300

    solution_v1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+  const styleLines = sourceString
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0);
+
+  for (const str of styleLines) {
+    let [key, value] = str.split(':', 2);
+
+    if (value) {
+      value = value.trim();
+    }
+    key = key.trim();
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
